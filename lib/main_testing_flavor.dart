d8a631faf6c384b8aeef0bde0688347efcd4926c
Igor Fedoseev <ifedoseev.flagman@gmail.com>
2023-07-15 01:01:02 +0200

    firebase tools installed

--- lib/main_testing_flavor.dart
+++ lib/main_testing_flavor.dart
@@ -1,12 +1,18 @@
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:ya_todo_list/flavors_config/config_provider.dart';
 import 'package:ya_todo_list/flavors_config/todo_config.dart';
 import 'package:ya_todo_list/my_app.dart';
 
+//import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   final config = TodoConfig(isTesting: true);
   runApp(
     ConfigProvider(
