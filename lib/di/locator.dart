410268a8b99f2d8a2174fecf7e84af1bf06bde3c
Igor Fedoseev <ifedoseev.flagman@gmail.com>
2023-07-15 21:47:12 +0200

    added crashlytics and get_it packages

---
+++ lib/di/locator.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import '../firebase_options.dart';
+
+GetIt locator = GetIt.instance;
+
+void setupLocator() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+}
--- lib/main_prod_flavor.dart
+++ lib/main_prod_flavor.dart
@@ -1,18 +1,11 @@
-//import 'package:firebase_core/firebase_core.dart';
+import 'di/locator.dart' as locator;
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:ya_todo_list/flavors_config/config_provider.dart';
 import 'package:ya_todo_list/flavors_config/todo_config.dart';
 import 'package:ya_todo_list/my_app.dart';
 
-//import 'firebase_options.dart';
-
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  locator.setupLocator();
   final config = TodoConfig(isTesting: false);
   runApp(
     ConfigProvider(
